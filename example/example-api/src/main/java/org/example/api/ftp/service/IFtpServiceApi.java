49d0d04a8f5cf430531961a173560877125da3a0
zhengqiaoqiao <302720700@qq.com>
2016-11-28 22:12:30 +0800

    少提交的内容

---
+++ example/example-api/src/main/java/org/example/api/ftp/service/IFtpServiceApi.java
@@ -0,0 +1,56 @@
+package org.example.api.ftp.service;
+
+import java.io.InputStream;
+
+
+public interface IFtpServiceApi {
+
+	/**
+	 * ftp上传
+	 * @param in
+	 * @param ftpServerPath
+	 * @param fileName
+	 * @return
+	 * @throws Exception 
+	 */
+	public boolean uploadFile(InputStream in, String ftpServerPath, String fileName) throws Exception;
+	
+	/**
+	 * ftp删除
+	 * @param fileName
+	 * @param ftpServerPath
+	 * @return
+	 * @throws Exception 
+	 */
+	public boolean deleteFile(String fileName, String ftpServerPath) throws Exception;
+	
+	
+	/**
+	 * 上传文件分片
+	 * @param in
+	 * @param ftpServerPath
+	 * @param fileMD5
+	 * @param fileSplitNo
+	 * @return
+	 * @throws Exception 
+	 */
+	public boolean uploadFileSplit(InputStream in, String ftpServerPath, String fileMD5, int fileSplitNo) throws Exception;
+	
+	/**
+	 * 合并文件分片
+	 * @param ftpServerPath
+	 * @param fileMD5
+	 * @param fileName
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean mergeFileSplit(String ftpServerPath, String fileMD5, String fileName) throws Exception;
+	/**
+	 * 判断文件是否存在
+	 * @param ftpServerPath
+	 * @param fileName
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean checkFileExist(String ftpServerPath, String fileName) throws Exception;
+}
